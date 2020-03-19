bb545ad4a93adbc9052b1c7a5d5d9a82baf39552
allger <407381392@qq.com>
2020-03-19 08:47:45 +0800

    static proxy

---
+++ src/main/java/com/study/staticproxy/ProfilingAspect.java
@@ -0,0 +1,27 @@
+package com.study.staticproxy;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.util.StopWatch;
+
+@Aspect
+public class ProfilingAspect {
+
+
+    @Around("methodsToBeProfiled()")
+    public Object profile(ProceedingJoinPoint pjp) throws Throwable {
+        StopWatch sw = new StopWatch(getClass().getSimpleName());
+        try {
+            sw.start(pjp.getSignature().getName());
+            return pjp.proceed();
+        } finally {
+            sw.stop();
+            System.out.println(sw.prettyPrint());
+        }
+    }
+    //com.study..service.*.*(..)
+    @Pointcut("execution(* com.study..service.*.*(..))")
+    public void methodsToBeProfiled(){}
+}
